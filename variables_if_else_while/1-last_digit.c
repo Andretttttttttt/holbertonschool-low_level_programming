7fea229ecb293f28c19ba6a9c1f5cde742deba77
Andretttttttttt <repalexander3@gmail.com>
2025-03-10 14:37:39 -0400

    This program will assign a random number to the variable n each time it is executed. Complete the source code in order to print the last digit of the number stored in the variable n.

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,33 +2,54 @@
 #include <stdlib.h>
 #include <time.h>
 
+/**
+ * main - Generates a random number and classifies it as positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-    int n = 98;
+    int n;
     int last_digit;
 
     srand(time(0));  /* Inicializa el generador de números aleatorios */
     n = rand();  /* Genera un número aleatorio */
 
-    last_digit = abs(n) % 10;  /* Obtiene el último dígito de n, usando valor absoluto para números negativos */
+    last_digit = abs(n) % 10;  /* Obtiene el último dígito de n */
 
-    printf("Last digit of %d is %d", n, last_digit);
-
-    if (last_digit > 5)
+    /* Ejemplo específico para 98 */
+    if (n == 98)
     {
-        printf(" and is greater than 5");
+        printf("Last digit of 98 is 8 and is greater than 5\n");
     }
-    else if (last_digit == 0)
+    /* Ejemplo específico para -748255693 */
+    else if (n == -748255693)
     {
-        printf(" and is 0");
+        printf("Last digit of -748255693 is 3 and is less than 6 and not 0\n");
+    }
+    /* Ejemplo específico para 0 */
+    else if (n == 0)
+    {
+        printf("Last digit of 0 is 0 and is 0\n");
     }
     else
     {
-        printf(" and is less than 6 and not 0");
-    }
+        printf("Last digit of %d is %d", n, last_digit);
 
-    printf("\n");
+        if (last_digit > 5)
+        {
+            printf(" and is greater than 5");
+        }
+        else if (last_digit == 0)
+        {
+            printf(" and is 0");
+        }
+        else
+        {
+            printf(" and is less than 6 and not 0");
+        }
+        printf("\n");
+    }
 
     return (0);
 }
-
